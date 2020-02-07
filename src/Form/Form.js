bff319c0ebe00842df4510288f38ffac225bcfc6
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-07 11:46:48 -0700

    Build out Form component skeleton

---
+++ src/Form/Form.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './Form.scss';
+
+export default class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            diet: '',
+            fun_fact: ''
+        };
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    render() {
+        return (
+            <form className='form'>
+                <input 
+                    type='text'
+                    placeholder='Name...'
+                    value={this.state.name}
+                    name='name'
+                    onChange={this.handleChange}
+                />
+                <input
+                    type='text'
+                    placeholder='Diet...'
+                    value={this.state.diet}
+                    name='diet'
+                    onChange={this.handleChange}
+                />
+                <input
+                    type='text'
+                    placeholder='Fun Fact...'
+                    value={this.state.fun_fact}
+                    name='fun_fact'
+                    onChange={this.handleChange}
+                />
+                <button
+                    onClick={this.submitNewCritter}
+                >
+                    Rescue!
+                </button>
+            </form>
+        )
+    }
+}
